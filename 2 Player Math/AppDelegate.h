02975cdcbc1c3dac0f4550b6d97733b9776ebed2
Arsalan Vakili <arsenal.v.001@gmail.com>
2015-08-10 11:19:29 -0400

    Initial Commit

---
+++ 2 Player Math/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  2 Player Math
+//
+//  Created by Arsalan Vakili on 2015-08-10.
+//  Copyright (c) 2015 Arsalan Vakili. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
